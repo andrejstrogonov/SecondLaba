ff435f5a29e3c1a4c1a8eae69aefdc8785bd68f4
Андрей Строгонов <strogonovandrej@hotmail.com>
2021-11-03 20:53:50 +0300

    Create transition to second activity(table)

--- app/src/main/java/com/home/secondlaba/MainActivity.kt
+++ app/src/main/java/com/home/secondlaba/MainActivity.kt
@@ -1,11 +1,19 @@
 package com.home.secondlaba
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val button: Button =findViewById(R.id.btn_login)
+        button.setOnClickListener{
+            val secondIntent=Intent(this,TableActivity::class.java)
+            startActivity(secondIntent)
+        }
     }
+
 }
