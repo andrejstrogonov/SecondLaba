0c51342bad543c7c69eeaaa7f937fe4a2a4a6bea
Андрей Строгонов <strogonovandrej@hotmail.com>
2021-11-03 19:48:40 +0300

    Create table activity

---
+++ app/src/main/java/com/home/secondlaba/TableActivity.kt
@@ -0,0 +1,11 @@
+package com.home.secondlaba
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class TableActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_table_activity)
+    }
+}
