b6c413a1f5992ac216bdcf75c4f28f63ce5b66af
Андрей Строгонов <strogonovandrej@hotmail.com>
2021-11-19 18:10:23 +0300

    Полный проект

--- app/src/main/java/com/home/secondlaba/TableActivity.kt
+++ app/src/main/java/com/home/secondlaba/TableActivity.kt
@@ -2,10 +2,27 @@ package com.home.secondlaba
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 class TableActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_table_activity)
+        val recyclerView:RecyclerView=findViewById(R.id.recyclerView)
+        recyclerView.layoutManager=LinearLayoutManager(this)
+        recyclerView.adapter = CustomRecyclerAdapter(fillList())
+        recyclerView.adapter = CustomRecyclerAdapter(getCatList())
+        recyclerView.layoutManager = LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false)
     }
+
+    private fun fillList(): List<String> {
+        val data = mutableListOf<String>()
+        (0..30).forEach { i -> data.add("\$i element") }
+        return data
+    }
+    private fun getCatList(): List<String> {
+        return this.resources.getStringArray(R.array.cat_names).toList()
+    }
+
 }
