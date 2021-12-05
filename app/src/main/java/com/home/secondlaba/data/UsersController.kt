c1612ddbf99c7510d7038f8e31e8c4628f08afb8
Андрей Строгонов <strogonovandrej@hotmail.com>
2021-12-05 22:24:24 +0300

    Finish preparation

--- app/src/main/java/com/home/secondlaba/data/UsersController.kt
+++ app/src/main/java/com/home/secondlaba/data/UsersController.kt
@@ -2,17 +2,11 @@ package com.home.secondlaba.data
 
 import com.home.secondlaba.api.UsersApiService
 import io.reactivex.Single
-import retrofit2.Retrofit
-import retrofit2.converter.gson.GsonConverterFactory
 
 class UsersController:UsersApiService {
-    val retrofit = Retrofit.Builder()
-        .addConverterFactory(GsonConverterFactory.create())
-        .baseUrl("https://reqres.in/api/users?page=2")
-        .build()
 
-    val usersService = retrofit.create(UsersApiService::class.java)
+
     override fun discoverAllUsers(): Single<UserListResponse> {
-        TODO("Not yet implemented")
+        TODO()
     }
 }
--- app/src/main/java/com/home/secondlaba/gui/TableActivity.kt
+++ app/src/main/java/com/home/secondlaba/gui/TableActivity.kt
@@ -4,11 +4,14 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.GridLayoutManager
 import com.home.secondlaba.R
+import com.home.secondlaba.api.UsersApiService
 import com.home.secondlaba.databinding.ActivityTableActivityBinding
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 
 
 class TableActivity : AppCompatActivity() {
-
+   lateinit var usersApiService: UsersApiService
     lateinit var binding: ActivityTableActivityBinding
     private val adapter= FilmAdapter()
     private val imageIdList=listOf(
@@ -23,13 +26,26 @@ class TableActivity : AppCompatActivity() {
         binding= ActivityTableActivityBinding.inflate(layoutInflater)
         setContentView(binding.root)
         init()
+        configureRetrofit()
     }
+
+    private fun configureRetrofit() {
+        val retrofit = Retrofit.Builder()
+            .addConverterFactory(GsonConverterFactory.create())
+            .baseUrl("https://reqres.in/api/users?page=2")
+            .build()
+
+        val usersService = retrofit.create(UsersApiService::class.java)
+        usersApiService=retrofit.create(UsersApiService::class.java)
+    }
+
 
     private fun init(){
         binding.apply {
             filmView.layoutManager= GridLayoutManager(this@TableActivity,3)
             filmView.adapter=adapter
             buttonAdd.setOnClickListener {
+
                 /*if(index>3) index=0
                 val movie=Film(imageIdList[index],"Movie $index")
                 adapter.addFilm(movie)
