90eb5e2c29587efb883f5378756fcfedb8626f73
Андрей Строгонов <strogonovandrej@hotmail.com>
2021-12-05 20:10:05 +0300

    Correct https data

--- app/src/main/java/com/home/secondlaba/api/UsersApiService.kt
+++ app/src/main/java/com/home/secondlaba/api/UsersApiService.kt
@@ -1,9 +1,9 @@
 package com.home.secondlaba.api
 
 import com.home.secondlaba.data.UserListResponse
-import retrofit2.http.GET
 import io.reactivex.Single
+import retrofit2.http.GET
 interface UsersApiService {
-    @GET("./api/users?page=2")
+    @GET("https://reqres.in/api/users?page=2")
     fun discoverAllUsers():Single<UserListResponse>
 }
